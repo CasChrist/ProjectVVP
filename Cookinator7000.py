2946fcb3cfaadc2fe761f03e1d2fc8838a1e120b
CasChrist <sozurukovmaksim@gmail.com>
2022-11-03 13:07:21 +0700

    Example bot

--- Cookinator7000.py
+++ Cookinator7000.py
@@ -1,2 +1,36 @@
-class Cookinator7000:
-	pass
+import logging
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I didn't recognize your input!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5440898247:AAF8kPh1qmxaL11Zsb1LA90Nh1Q2H5YPX10').build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(unknown_handler)
+    
+    application.run_polling()
