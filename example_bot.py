eb911143616503b9a7e8c8812d999371f39f71b2
CasChrist <sozurukovmaksim@gmail.com>
2022-11-12 21:47:54 +0700

    Added example buttons

--- example_bot.py
+++ example_bot.py
@@ -3,12 +3,14 @@
 """
 
 # импорт зависимых библиотек
-import eb_config, logging
-from telegram inport Update
-from telegram.ext import ApplicationHandler, CommandHandler, MessageHandler, Filters
+import eb_config, logging, eb_btn_menu
+from eb_btn_menu import facts_to_str, regular_choice, custom_choice, received_information, done, CHOOSING, TYPING_REPLY, TYPING_CHOICE, reply_keyboard, markup
+from telegram import Update, KeyboardButton
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ConversationHandler, filters
 
 # подключаем журнал аудита (лог действий)
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def start(update, context):
     # ожидание отправки сообщения по сети - нужен оператор 'await'
@@ -39,22 +41,43 @@ async def unknown (update, context):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(eb_config.TOKEN).build()
     
-    # при получении запроса '/start' требуется вызвать функцию 'start()'
-    start_handler = CommandHandler('start', start)
+    """ при получении запроса '/start' требуется вызвать функцию 'start()'
+    start_handler = CommandHandler('start', start) """
     
     # при получении сообщения без '/' требуется вывести пользователю его же сообщение
-    echo_handler = MessageHandler(Filters.TEXT & (~Filters.COMMAND), echo)
+    #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     
     # при получении запроса '/caps (arguments)' требуется вызвать функцию 'caps()'
     caps_handler = CommandHandler('caps', caps)
-    
+
+    # кнопочки
+    conversation_handler = ConversationHandler(
+        # точка входа в разговор
+        entry_points = [CommandHandler("start", eb_btn_menu.start)],
+        # словарь состояний разговора, возвращаемых callback функциями
+        states = {
+            # этап 'CHOOSING' - т.е. функция-обработчик какого-то сообщения явно
+            # вернула константу 'CHOOSING' (return 'CHOOSING'), а так же послала/ответила
+            # на сообщение. Ответ пользователя на это сообщение будет
+            # обрабатываться хэндлерами, определёнными в этом списке
+            CHOOSING: [
+                    MessageHandler(filters.Regex("^(Age|Favourite colour|Number of siblings)$"), regular_choice),
+                    MessageHandler(filters.Regex("^Something else...$"), custom_choice)],
+            TYPING_CHOICE: [
+                MessageHandler(filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), regular_choice)],
+            TYPING_REPLY: [
+                MessageHandler(filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), received_information)]},
+        # точка выхода из разговора
+        fallbacks = [MessageHandler(filters.Regex("^Done$"), done)])
+
     # при получении неизвестного запроса требуется вызвать функцию 'unknown()'
-    unknown_handler = MessageHandler(Filters.command, unknown)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
     
     # добавяем все хэндлеры в приложение
-    app.add_handler(start_handler)
-    app.add_handler(echo_handler)
+    #app.add_handler(start_handler)
+    #app.add_handler(echo_handler)
     app.add_handler(caps_handler)
+    app.add_handler(conversation_handler)
     
     # хэндлер неизвестных запросов добавлять в самом конце!
     app.add_handler(unknown_handler)
