09ef0c9991e27dee1de5754f6bdc1d4c5c120026
Altasa <mehanu.kea@gmail.com>
2022-12-14 15:10:31 +0700

    Integral Function v2.3

--- gen_buttons.py
+++ gen_buttons.py
@@ -4,10 +4,6 @@ from proreceip import keys
 buttons=[]
 subcategory_content=[]
 j=0
-def save_category(subcategory_content, j):
-    buttons.append(subcategory_content)
-    j=j+1
-    subcategory_content=[]
 for i in range(len(keys)):
     subcategory_content.append(keys[i])
     if (i==18)or(i==50)or(i==75)or(i==92)or(i==114)or(i==145)or(i==155)or(i==180)or(i==215)or(i==224)or(i==229)or(i==244)or(i==247)or(i==257)or(i==276)or(i==283)or(i==305)or(i==316) :
