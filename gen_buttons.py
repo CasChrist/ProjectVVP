519fc859baece6fe57d584e7720097300ce3cb96
Altasa <mehanu.kea@gmail.com>
2022-12-15 23:39:31 +0700

    Minor Hotfixes 014

--- gen_buttons.py
+++ gen_buttons.py
@@ -6,7 +6,7 @@ subcategory_content=[]
 j=0
 for i in range(len(keys)):
     subcategory_content.append(keys[i])
-    if (i==18)or(i==27)or(i==35)or(i==37)or(i==54)or(i==75)or(i==85)or(i==108)or(i==141)or(i==155)or(i==158)or(i==162)or(i==164)or(i==171)or(i==182) :
+    if (i==18)or(i==27)or(i==35)or(i==37)or(i==54)or(i==75)or(i==85)or(i==108)or(i==141)or(i==155)or(i==158)or(i==162)or(i==164)or(i==171)or(i==181) :
         buttons.append(subcategory_content)
         j=j+1
         subcategory_content=[]
